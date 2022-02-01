59497ba21a3ec80a3203ff17287ca3d57e5fa06e
SchmidtFred <fschmidt@live.com>
2022-02-01 15:24:38 -0600

    (+)ItemInputList renders, ListTemplates create, boardSquareTemplates create

---
+++ src/components/listCreate/ItemInputList.js
@@ -0,0 +1,19 @@
+//This module is for styling and controlling the listItem inputs
+import React from "react";
+import { ItemInput } from "./ItemInput";
+
+export const ItemInputList = ({listItems, setListItems}) => {
+
+    return (
+        <>
+            <section className="inputList">
+                <h2>What Goes In Your Squares</h2>
+                {listItems.length > 0 ? listItems.map(item => {
+                           return <div className="inputList__item" key={item.tempId}>
+                               <ItemInput  listItem={item} listItemsArray={listItems} setListItemsArray={setListItems} />
+                           </div>
+                }) : null}
+            </section>
+        </>
+    )
+}
